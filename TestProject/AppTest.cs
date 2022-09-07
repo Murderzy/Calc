114f5a835f4f3e42e1a0f88dd33d77391fbd3faa
Murderzy <anntarasiuk@gmail.com>
2022-09-07 09:39:49 +0300

    Edit notes && test

--- TestProject/AppTest.cs
+++ TestProject/AppTest.cs
@@ -15,18 +15,37 @@ namespace TestProject
         }
 
         [TestMethod]
-        public void RomanNumberParse()
+        public void RomanNumberParse1Digit()
         {
-            Assert.AreEqual(RomanNumber.Parse("I"), 1, "I == 1");
-            Assert.AreEqual(RomanNumber.Parse("IV"), 4, "IV == 4");
-            Assert.AreEqual(RomanNumber.Parse("XV"), 15);
-            Assert.AreEqual(RomanNumber.Parse("XXX"), 30);
-            Assert.AreEqual(RomanNumber.Parse("CM"), 900);
-            Assert.AreEqual(RomanNumber.Parse("MCMXCIX"), 1999);
-            Assert.AreEqual(RomanNumber.Parse("CD"), 400);
-            Assert.AreEqual(RomanNumber.Parse("CDI"), 401);
-            Assert.AreEqual(RomanNumber.Parse("LV"), 55);
-            Assert.AreEqual(RomanNumber.Parse("XL"), 40);
+            Assert.AreEqual(1,RomanNumber.Parse("I"), "I == 1");
+           
+        }
+
+        [TestMethod]
+        public void RomanNumberParse2Digit()
+        {
+            
+            Assert.AreEqual(4,RomanNumber.Parse("IV"), "IV == 4");
+            Assert.AreEqual(15, RomanNumber.Parse("XV"), "XV == 15");
+            
+            Assert.AreEqual(900,RomanNumber.Parse("CM"), "CM == 900");
+           
+            Assert.AreEqual(400,RomanNumber.Parse("CD"), "CD == 400");
+            
+            Assert.AreEqual(55,RomanNumber.Parse("LV"), "LV == 55");
+            Assert.AreEqual(40,RomanNumber.Parse("XL"), "XL == 40");
+        }
+
+        [TestMethod]
+        public void RomanNumberParse3Digit()
+        {
+           
+            Assert.AreEqual(30,RomanNumber.Parse("XXX"), "XXX == 30");
+           
+            Assert.AreEqual(1999,RomanNumber.Parse("MCMXCIX"), "MCMXCIX == 1999");
+            
+            Assert.AreEqual(401,RomanNumber.Parse("CDI"), "CDI == 4");
+            
         }
     }
 }
