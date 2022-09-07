34df961cdc6fcac76c8161ed817355495d99d6e8
Murderzy <anntarasiuk@gmail.com>
2022-09-07 10:44:10 +0300

    edit test

--- TestProject/AppTest.cs
+++ TestProject/AppTest.cs
@@ -18,7 +18,8 @@ namespace TestProject
         public void RomanNumberParse1Digit()
         {
             Assert.AreEqual(1,RomanNumber.Parse("I"), "I == 1");
-           
+            Assert.AreEqual(5, RomanNumber.Parse("V"), "I == 5");
+
         }
 
         [TestMethod]
@@ -46,6 +47,32 @@ namespace TestProject
             
             Assert.AreEqual(401,RomanNumber.Parse("CDI"), "CDI == 4");
             
+        }
+
+        [TestMethod]
+        public void RomanNumberInvalidDigit()
+        {
+
+            //с некоректной цифрой должен выдать исключение
+            //Assert.AreEqual(0, RomanNumber.Parse("XXA")); - doesn't work -- fail test -- наше исключение воспринимается как ошибка
+            //Сравнение типов исключений суровое 
+            //Если ожидаемого исключение не будет, то и тест считается непройденным
+            var exc = Assert.ThrowsException<ArgumentException>(() => { RomanNumber.Parse("XXA"); });
+            //сохраняем исключение,которое возникает во время тестирования
+            var exp = new ArgumentException("Invalid char A");
+            //ожидаемое исключение с предыдущего теста
+
+            Assert.AreEqual(exp.Message, exc.Message);
+
+            //Act()-выкидывает исколючение как это тестировать
+            //Assert.SomeTest(Act()) - имеем вызов Act(), как следствие выброс исключения
+            //как следствие остановка Assert.SomeTest() и провал теста как следствие
+            //По этому действие с исключением оборачивают лямбдой (Action) и её передают на тест
+            //Assert.SomeTest(()=>Act())
+            //Assert.SomeTest(new Action(Act))
+            //Аргумент метода SomeTest() - это действие, которое ещё не выполняется, в отличии от SomeTest(Act())
+            //а значит это действие может быть выполненно средствами тестирования , в том числе с catch
+
         }
     }
 }
